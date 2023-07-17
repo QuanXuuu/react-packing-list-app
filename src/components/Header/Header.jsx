33044c8e0cc394638bc99b177186a749176b811e
Quan Xu <xuquan69220@gmail.com>
2023-07-17 13:29:44 +0200

    Update color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.scss";
 export const Header = () => {
   return (
     <div className="header">
-      <h1>🏝On the way🧳</h1>
+      <h1>On the way</h1>
       <h2> Your Personal Packing List</h2>
     </div>
   );

ce1eabffa584cd66b7bcb5bdaa38b2331515f00f
Quan Xu <xuquan69220@gmail.com>
2023-07-17 19:00:53 +0200

    Update resposive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,13 @@ import "./Header.scss";
 export const Header = () => {
   return (
     <div className="header">
-      <h1>On the way</h1>
-      <h2> Your Personal Packing List</h2>
+      <h1>🏝️ On the way</h1>
+      <div className="header-profile">
+        <img
+          src="https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+      </div>
     </div>
   );
 };
